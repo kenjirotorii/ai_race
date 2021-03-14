1585823b9958bed24c382b0b681ab6af6fe57b94
Kenjiro Torii <kenjiro.recruit@gmail.com>
2021-03-14 13:33:33 +0900

    [add] variational autoencoder

--- ai_race/your_environment/scripts/autoencoder.py
+++ ai_race/your_environment/scripts/autoencoder.py
@@ -8,10 +8,10 @@ def conv2d_size_out(size, kernel_size=5, stride=2):
 
 
 class Encoder(nn.Module):
-
-    def __init__(self, h, w, outputs):
+    def __init__(self, h, w, outputs, variational=False):
         super(Encoder, self).__init__()
 
+        self.variational = variational
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 16, kernel_size=5, stride=2)
@@ -22,14 +22,25 @@ class Encoder(nn.Module):
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         linear_input_size = convw * convh * 32
-        self.head = nn.Linear(linear_input_size, outputs)
+
+        if self.variational:
+            self.mu_head = nn.Linear(linear_input_size, outputs)
+            self.lnvar_head = nn.Linear(linear_input_size, outputs)
+        else:
+            self.head = nn.Linear(linear_input_size, outputs)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        x = self.head(x.view(x.size(0), -1))
-        return x
+        x = x.view(x.size(0), -1)
+        if self.variational:
+            mu = self.mu_head(x)
+            lnvar = self.lnvar_head(x)
+            return mu, lnvar
+        else:
+            x = self.head(x)
+            return x
 
 
 class Decoder(nn.Module):
@@ -65,13 +76,32 @@ class Autoencoder(nn.Module):
     def __init__(self, h, w, outputs):
         super(Autoencoder, self).__init__()
 
-        self.encoder = Encoder(h, w, outputs)
+        self.encoder = Encoder(h, w, outputs, False)
         self.decoder = Decoder(outputs, h, w)
         
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
+
+
+class VAE(nn.Module):
+    def __init__(self, h, w, outputs):
+        super(VAE, self).__init__()
+
+        self.encoder = Encoder(h, w, outputs, True)
+        self.decoder = Decoder(outputs, h, w)
+    
+    def reparameterize(self, mu, lnvar):
+        std = torch.exp(0.5*lnvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def forward(self, x):
+        mu, lnvar = self.encoder(x)
+        x = self.reparameterize(mu, lnvar)
+        x = self.decoder(x)
+        return x, mu, lnvar
 
 
 class ControlHead(nn.Module):
@@ -94,20 +124,29 @@ if __name__ == "__main__":
     w = 320
     img = torch.rand(2, 3, h, w)
     z = 64
+    variational = True
 
-    encoder = Encoder(h, w, z)
-    decoder = Decoder(z, h, w)
-    ae = Autoencoder(h, w, z)
+    if variational:
+        mod = VAE(h, w, z)
+    else:
+        mod = Autoencoder(h, w, z)
 
     def example(model, x):
         print(model)
         model.eval()
-        y = model(x)
+        if variational:
+            y, _, _ = model(x)
+        else:
+            y = model(x)
         print(y.size())
     
-    def transfer():
-        model_new = Autoencoder(h, w, z)
-        model_new.load_state_dict(ae.state_dict())
+    def transfer(model, x):
+        if variational:
+            model_new = VAE(h, w, z)
+        else:
+            model_new = Autoencoder(h, w, z)
+
+        model_new.load_state_dict(model.state_dict())
 
         for name, param in model_new.named_parameters():
             layer_name = name.split('.')[0]
@@ -121,8 +160,12 @@ if __name__ == "__main__":
             print(name, param.requires_grad)
 
         model_new.eval()
-        y = model_new(img)
+        if variational:
+            y, _, _ = model_new(x)
+        else:
+            y = model_new(x)
+
         print(y.size())
     
-
-    transfer()
+    # example(mod, img)
+    transfer(mod, img)
